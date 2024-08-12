8defbe2d3d8e56559d5ef052533ef513a0e5170f
koribillRamudu <sekharramudu9054@msitprogram.net>
2024-08-12 18:54:21 +0530

    day1 assignment

---
+++ Day1/AssignmentDay1.js
@@ -0,0 +1,81 @@
+// Assignment day write five examples for data types,operators automatic types conversion
+
+// Datatypes:
+// 1.numbers:
+console.log(10)
+console.log(20.0)
+console.log(-1)
+console.log(10/0) 
+console.log(-10/0)
+console.log(0/0)
+
+// 2.string in this there are "",'',``
+console.log("3"+"2")
+console.log("3"+2)
+console.log('hello'+'world')
+console.log(`hello welcome to day 
+one class`)
+
+let a=10;
+console.log(`The a value is ${a}`)
+
+// 3.Boolean
+console.log(10==10)
+console.log(2>3)
+console.log(true==1)
+console.log(!true)
+console.log("cat" =="dog")
+
+// 4.Empty values
+let n;
+console.log(n)
+let g=null;
+console.log(g)
+
+// operators
+// 1.Arithmatic operator
+console.log(10+10)
+console.log(20-2)
+console.log(3*4)
+console.log(9/3)
+console.log(9%2)
+console.log("3"+2)
+console.log("3"-2)
+console.log("-3"*2)
+
+// 2.unary operator
+let d = "15";
+d = +d;
+console.log(d)
+let w="2";
+w=-w;
+console.log(w)
+let e = true;
+e = !e;
+console.log(e)
+
+// 3.logical operator
+let result =true && false
+console.log(result)
+let r=true || false
+console.log(r)
+let q= !true
+console.log(q)
+
+// 4.terinary
+console.log(10 ? "Hello" : "bye")
+console.log(typeof "hello")
+console.log(  null ?? "hi")
+console.log(10,20)
+console.log(10**2)
+
+// automatic type conversion
+console.log("3"+2)
+console.log(100-"1")
+console.log("-3.1"*3)
+console.log(true +1)
+console.log("true"+"1")
+
+
+
+
