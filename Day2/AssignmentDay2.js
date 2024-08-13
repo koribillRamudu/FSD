d8ef9107f71e768b6255325b9f0a855ec7e040b0
koribillRamudu <sekharramudu9054@msitprogram.net>
2024-08-13 15:20:46 +0530

    Assignment day2

---
+++ Day2/AssignmentDay2.js
@@ -0,0 +1,32 @@
+console.log('10'+5)
+console.log('10'-5)
+console.log(true+2)
+console.log(false +undefined)
+
+console.log(0==false)
+console.log(0===false)
+console.log(''==0)
+console.log(''===0)
+
+
+console.log(0||1 && 2|| 3)
+console.log(false||(true && false)|| true)
+console.log(0 && 1 || 2 && 3)
+
+let a=10,b=20,c=30
+console.log(a+b*c)
+console.log((a+b)*c)
+console.log(a+b >c ? a:b)
+console.log((a>b)&&(b>c) || (a>c))
+
+console.log([]+{})
+console.log({}+[])
+console.log([]==![])
+console.log(''==[])
+
+
+console.log(+"")
+console.log(+true)
+console.log(+false)
+console.log(+null)
+console.log(+undefined)
